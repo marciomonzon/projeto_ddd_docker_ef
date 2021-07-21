434dba016e2a3253ca0fd6eb9dd989d5194a9e87
Marcio <marcio.pcmonzon@gmail.com>
2021-07-20 21:23:17 -0300

    mais avanços

---
+++ RestApiModeloDDD/RestApiModeloDDD.Infrastructure/4.1.Data/SqlContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestApiModeloDDD.Domain.Entities;
+using System;
+using System.Linq;
+
+namespace RestApiModeloDDD.Infrastructure._4._1.Data
+{
+    public class SqlContext : DbContext
+    {
+        public SqlContext()
+        {
+        }
+
+        public SqlContext(DbContextOptions<SqlContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+        public override int SaveChanges()
+        {
+            var expression = ChangeTracker.Entries()
+                .Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null);
+
+            foreach (var entry in expression)
+            {
+                if (entry.State == EntityState.Added)
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+                if (entry.State == EntityState.Modified)
+                    entry.Property("DataCadastro").IsModified = false;
+            }
+            return base.SaveChanges();
+        }
+    }
+}
