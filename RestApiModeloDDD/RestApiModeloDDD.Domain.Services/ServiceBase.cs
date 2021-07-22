f1616b8eaf6a5797ff5d2a130650efd396b0aba9
Marcio <marcio.pcmonzon@gmail.com>
2021-07-21 21:03:55 -0300

    repositorios e services

---
+++ RestApiModeloDDD/RestApiModeloDDD.Domain.Services/ServiceBase.cs
@@ -0,0 +1,41 @@
+﻿using RestApiModeloDDD.Domain.Core.Interfaces.Repositories;
+using RestApiModeloDDD.Domain.Core.Interfaces.Services;
+using System.Collections.Generic;
+
+namespace RestApiModeloDDD.Domain.Services
+{
+    public class ServiceBase<TEntity> : IServiceBase<TEntity> where TEntity : class
+    {
+        private readonly IRepositoryBase<TEntity> _repositoryBase;
+
+        public ServiceBase(IRepositoryBase<TEntity> repositoryBase)
+        {
+            _repositoryBase = repositoryBase;
+        }
+
+        public void Add(TEntity obj)
+        {
+            _repositoryBase.Add(obj);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _repositoryBase.GetAll();
+        }
+
+        public TEntity GetById(int id)
+        {
+            return _repositoryBase.GetById(id);
+        }
+
+        public void Remove(TEntity obj)
+        {
+            _repositoryBase.Remove(obj);
+        }
+
+        public void Update(TEntity obj)
+        {
+            _repositoryBase.Update(obj);
+        }
+    }
+}
