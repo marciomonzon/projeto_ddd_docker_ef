3e9044d3aed68b71215724e038a0daa272ff9895
Marcio <marcio.pcmonzon@gmail.com>
2021-08-14 16:48:45 -0300

    more coding

---
+++ RestApiModeloDDD/RestApiModeloDDD.Application/ApplicationServiceProduto.cs
@@ -0,0 +1,51 @@
+﻿using RestApiModeloDDD.Application.Dtos;
+using RestApiModeloDDD.Application.Interfaces;
+using RestApiModeloDDD.Application.Interfaces.Mapper;
+using RestApiModeloDDD.Domain.Core.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+
+namespace RestApiModeloDDD.Application
+{
+    public class ApplicationServiceProduto : IApplcationServiceProduto
+    {
+        private readonly IServiceProduto _serviceProduto;
+        private readonly IMapperProduto _mapperProduto;
+
+        public ApplicationServiceProduto(IServiceProduto serviceProduto, IMapperProduto mapperProduto)
+        {
+            _serviceProduto = serviceProduto;
+            _mapperProduto = mapperProduto;
+        }
+
+        public void Add(ProdutoDto produto)
+        {
+            var entidade = _mapperProduto.MapperDtoToEntity(produto);
+            _serviceProduto.Add(entidade);
+        }
+
+        public IEnumerable<ProdutoDto> GetAll()
+        {
+            var produtos = _serviceProduto.GetAll();
+            return _mapperProduto.MapperListProdutosDto(produtos);
+        }
+
+        public ProdutoDto GetById(int id)
+        {
+            var produto = _serviceProduto.GetById(id);
+            return _mapperProduto.MapperEntityToDto(produto);
+        }
+
+        public void Remove(ProdutoDto produto)
+        {
+            var entidade = _mapperProduto.MapperDtoToEntity(produto);
+            _serviceProduto.Remove(entidade);
+        }
+
+        public void Update(ProdutoDto produto)
+        {
+            var entidade = _mapperProduto.MapperDtoToEntity(produto);
+            _serviceProduto.Update(entidade);
+        }
+    }
+}
---
+++ RestApiModeloDDD/RestApiModeloDDD.Infrastructure/CrossCutting/IOC/ConfigurationIOC.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using RestApiModeloDDD.Application;
+using RestApiModeloDDD.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestApiModeloDDD.Infrastructure.CrossCutting.IOC
+{
+    public class ConfigurationIOC
+    {
+        public static void Load(ContainerBuilder builder)
+        {
+            #region IOC
+
+            builder.RegisterType<ApplicationServiceCliente>().As<IApplicationServiceCliente>();
+            builder.RegisterType<ApplicationServiceProduto>().As<IApplcationServiceProduto>();
+
+            #endregion
+        }
+    }
+}
